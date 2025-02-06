2145470b7cc6461f23249551441f1282acf17bfe
Taleiven Kattan <taleiven@gmail.com>
2025-02-06 01:50:15 +0100

    Update versions to allow run

--- lib/game.dart
+++ lib/game.dart
@@ -70,7 +70,7 @@ class _GameState extends State<Game> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
