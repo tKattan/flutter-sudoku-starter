4058c7141593dff7c6a7a6287cfb0b2dfd64ee9d
tkattan <taleiven@gmail.com>
2025-02-12 22:55:05 +0000

    Resolves 1

--- lib/game.dart
+++ lib/game.dart
@@ -65,13 +65,21 @@ class _GameState extends State<Game> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            SizedBox(
+              height: boxSize*3,
+              width: boxSize*3,
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(9, (x) {
+                  return Container(
+                    width: boxSize,
+                    height: boxSize,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent),
+                    ),
+                  );
+                }),
+              ),)
           ],
         ),
       ),
